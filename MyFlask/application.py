aecb288fac7873bd8a14e9934fffb63e83f1aca1
Umran Yaman <umran.yaman.19@ucl.ac.uk>
2023-03-12 01:45:38 +0000

    first commit

---
+++ MyFlask/application.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import joblib
+application = Flask(__name__)
+
+vectorizer = joblib.load('vectorizer.pkl')
+spam_ham_model = joblib.load('spam_ham_model.pkl')
+
+# routing handler
+@application.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@application.route('/spamorham', methods=['GET', 'POST'])
+def spamorham():
+    message = request.args.get('message')
+    vect_message = vectorizer.transform([message])
+    result = spam_ham_model.predict(vect_message)[0]
+    return result
+
+# run the app
+if __name__ == '__main__':
+    application.run()
